cbc249be3cb3c683bac4e9c4c8e1dd173961867a
Tonguc Gokceer <tongucgokceer@gmail.com>
2020-10-29 18:17:35 +0300

    scaffold Post

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :posts
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #root to: "home#index"  #Will be defined after some progress...
+
 end
